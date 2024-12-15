b5cca8bdee0c7f7518e78454bcdeffa527c4edcc
kuzhanthaivel <kuzhanthaivel272@gmail.com>
2024-12-15 22:27:56 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+// Import Schema
+const { User, Admin, DrugData } = require('./Schema');
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const mongoUrl = process.env.MONGO_URL; 
+
+mongoose.connect(mongoUrl).then(() => {
+    console.log("MongoDB successfully connected");
+}).catch((e) => {
+    console.error("Failed to connect to MongoDB", e);
+});
+
+app.get("/", (req, res) => {
+    res.send("Hello, World!");
+});
+
+app.listen(5001, () => {
+    console.log('Server is running on port 5001');
+});
